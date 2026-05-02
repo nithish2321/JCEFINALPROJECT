5e68875dd5a922624063b9b342e616d50fee02f4
Nithish kumar <nithishkumarbcsmca@gmail.com>
2026-05-02 11:12:34 +0530

    opt commit

--- script.js
+++ script.js
@@ -13,12 +13,28 @@ console.log("a+b = "+(a+b));
 async function load(){
     try{
        const response = await fetch("https://nithish2321.github.io/API/api.json");
-       if(response.ok){
+       console.log(response);
+       let data = await response.json();
+        if(response.ok){
            console.log("data loaded successfully");
-           console.log(await response.json());
+           console.log(data);
        }else{
            console.log("data not loaded");
        }
+        
+       let category = data.meta.categories || [];
+        
+       const catgoriesS = document.getElementById("categories");
+        
+        
+        category.forEach((element) =>{
+            let opt = document.createElement("option");
+            opt.value = element;
+            opt.innerText = element;
+            catgoriesS.appendChild(opt);
+        });
+        
+       console.log(category);
         
     }catch(error){
         console.error(error.message);
