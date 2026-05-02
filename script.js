99e14086f13cec94bb926c7cad18efbf7034fa34
Nithish kumar <nithishkumarbcsmca@gmail.com>
2026-05-02 10:07:28 +0530

    second commit

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ why();
 let a = 10;
 let b = 20;
 console.log("a+b = "+(a+b));
+
+async function load(){
+    try{
+       const response = await fetch("https://nithish2321.github.io/API/api.json");
+       if(response.ok){
+           console.log("data loaded successfully");
+           console.log(await response.json());
+       }else{
+           console.log("data not loaded");
+       }
+        
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+load();
