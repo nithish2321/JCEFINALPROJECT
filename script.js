5c939539b20882b7830fb09f46526e66ed52633a
Nithish kumar <nithishkumarbcsmca@gmail.com>
2026-05-02 10:06:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+try{
+console.log("Hello");
+why();
+}catch(error){
+    console.log("Error Name: "+error.name);
+    console.log("Error Message: "+error.message);
+}
+
+let a = 10;
+let b = 20;
+console.log("a+b = "+(a+b));
